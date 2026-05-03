f761bbe840a65091987aeb4c8be01c7b5fe653a3
h3377 <139696940+h3377@users.noreply.github.com>
2026-05-03 08:19:13 +0300

    Create pointers and arrays.cpp

---
+++ Pointer/pointers and arrays.cpp
@@ -0,0 +1,38 @@
+// #40.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int arr[4] = { 10,20,30,40 };
+    int* ptr;
+    ptr= arr;
+
+    cout << "Addresses are :\n";
+
+    cout << ptr << endl;
+    cout << ptr + 1 << endl;
+    cout << ptr + 2 << endl;
+    cout << ptr + 3 << endl;
+
+
+    cout << "\nValues are : \n";
+
+   /** cout << *ptr << endl;
+    cout << *(ptr + 1)<< endl;
+    cout << *(ptr + 2) << endl;
+    cout << *(ptr + 3) << endl;*/
+
+    for (int i = 0; i <= 3; i++)
+    {
+        cout << *(ptr + i) << endl;
+    }
+
+    return 0;
+
+}
+
