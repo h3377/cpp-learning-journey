214855066aeb710259565c06d40a3b3a58a7c86c
h3377 <139696940+h3377@users.noreply.github.com>
2026-05-06 14:38:21 +0300

    Create Dynamic array.cpp

---
+++ dynamic memory Allocate/Dynamic array.cpp
@@ -0,0 +1,40 @@
+// #44 Dynamic arrays new and delete.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "enter total number of students: ";
+	cin >> num;
+
+	float* ptr;
+
+	ptr= new float[num];
+
+	cout << "enter grades of students." << endl;
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << "student" << i + 1;
+		cin >> *(ptr + 1);
+
+
+	}
+
+	cout << "\nDisplaying grades of students. " << endl;
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << "student" << i + 1 << ": " << *(ptr + 1) << endl;
+	}
+
+  //ptr memory is released
+	delete[] ptr;
+
+	return 0;
+
+}
+
