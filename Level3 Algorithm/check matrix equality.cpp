8a5a7694e37172ae378bed3c87ec8e143bd9a39c
h3377 <139696940+h3377@users.noreply.github.com>
2026-06-20 23:56:41 +0300

    Create check matrix equality.cpp

---
+++ Level3 Algorithm/check matrix equality.cpp
@@ -0,0 +1,75 @@
+// #11 II 3 CHECK  MATRICES Equality.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+#include<iomanip>
+
+using namespace std;
+
+int RandNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void fillMatrxWithRandomNumber(int arr[3][3], short Rows, short cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < cols; j++)
+        {
+            arr[i][j] = RandNumber(1, 10);
+        }
+    }
+}
+int sumOfMatrix(int arr[3][3], short Rows, short Col)
+{
+    int sum = 0;
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Col; j++)
+        {
+            
+            sum += arr[i][j];
+        }
+
+    }
+    return sum;
+}
+bool IsMatrixEqual(int Matrix1[3][3], int Matrix2[3][3], short Rows, short Col)
+{
+    return(sumOfMatrix(Matrix1, Rows, Col) == sumOfMatrix(Matrix2, Rows, Col));
+}
+void printMatrix(int arr[3][3], short Rows, short Cols)
+{
+
+
+    for (short i = 0; i < Rows; i++)
+    {
+        for (short j = 0; j < Cols; j++)
+        {
+            printf("%0*d    ", 2, arr[i][j]);
+        }
+        cout << "\n";
+    }
+
+}
+int main()
+{ 
+    srand((unsigned)time(NULL));
+    int Matrix[3][3],Matrix2[3][3];
+
+    fillMatrxWithRandomNumber(Matrix, 3, 3);
+    cout << "Matrix1:\n";
+    printMatrix(Matrix, 3, 3);
+    fillMatrxWithRandomNumber(Matrix2, 3, 3);
+    cout << "\nMatrix2:\n";
+    printMatrix(Matrix2, 3, 3);
+
+    if(IsMatrixEqual(Matrix,Matrix2,3,3))
+        cout << "\nYES: both martices are equal.";
+    else
+        cout << "\nNo: martices are NOT equal."; 
+
+    system("pause>0");
+}
