fc1f13d27f014a9d86fe5b540cf0aa1606c0fd96
h3377 <139696940+h3377@users.noreply.github.com>
2026-08-26 17:17:48 +0300

    Implement function to print each word in a string
    This program reads a string from the user and prints each word separately.

---
+++ Level3 Algorithm/Print each word in string.cpp
@@ -0,0 +1,48 @@
+// #35 II 3 Print each word in string.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadString()
+{
+    string S1;
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+    return S1;
+}
+void PrintEachWordInString(string S1)
+{
+    string delim = " "; // delimiter
+    cout << "\nYour string wrords are: \n\n";
+    short pos = 0;
+    string sWord; // define a string variable
+    // use find() function to get the position of the delimiters
+    while ((pos = S1.find(delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos); // store the word
+        if (sWord != "")
+        {
+            cout << sWord << endl;
+        }
+        S1.erase(0, pos + delim.length()); /* erase() until
+        positon and move to next word. */
+    }
+    if (S1 != "")
+    {
+        cout << S1 << endl; // it print last word of the string.
+    }
+}
+
+int main()
+{
+    PrintEachWordInString(ReadString());
+
+    system("pause>0");
+
+}
+
+   
+
